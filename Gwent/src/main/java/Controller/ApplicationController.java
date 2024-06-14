5d4d533cb3234d2258caead9dad7a26c8b84dde1
ali <ali>
2024-06-14 14:05:32 +0330

    for merge hamid

--- Gwent/src/main/java/Controller/ApplicationController.java
+++ Gwent/src/main/java/Controller/ApplicationController.java
@@ -1,6 +1,9 @@
 package Controller;
 
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -9,7 +12,7 @@ public class ApplicationController {
    private static Stage stage ;
    public static final int WIDTH =1280;
    public static final int HEIGHT =720;
-
+private static MediaPlayer mediaPlayer;
 
     public static Stage getStage() {
         return stage;
@@ -18,8 +21,31 @@ public class ApplicationController {
     public static void setStage(Stage stage) {
         ApplicationController.stage = stage;
     }
-     public static void setRootSize(Pane pane){
+
+    public static void setRootSize(Pane pane){
         pane.setPrefHeight(HEIGHT);
         pane.setPrefWidth(WIDTH);
      }
+     public static void setIcon(){
+         stage.getIcons().add(new Image(ApplicationController.class.getResourceAsStream("/someImages/icon.png")));
+     }
+     public static void setMedia(String mediaAddress){
+         if (ApplicationController.mediaPlayer == null) {
+             Media media = new Media(ApplicationController.class.getResource(mediaAddress).toExternalForm());
+             MediaPlayer mediaPlayer = new MediaPlayer(media);
+             mediaPlayer.play();
+             mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+             mediaPlayer.setVolume(0.1);
+             ApplicationController.mediaPlayer =mediaPlayer;
+         }
+     }
+     public static BackgroundImage setBackGround(String imagesAddress){
+         Image image = new Image(ApplicationController.class.getResourceAsStream(imagesAddress), WIDTH, HEIGHT, false, false);
+         BackgroundImage backgroundImage = new BackgroundImage(image,
+                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                 BackgroundPosition.CENTER,
+                 new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false));
+         return backgroundImage;
+     }
+
 }
--- Gwent/src/main/java/View/GameMenu.java
+++ Gwent/src/main/java/View/GameMenu.java
@@ -1,4 +1,31 @@
 package View;
 
-public class GameMenu {
+import Controller.ApplicationController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class GameMenu extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+       ApplicationController.setMedia("/music/along-the-wayside-medieval-folk-music-128697.mp3");
+        URL url = PreGameMenu.class.getResource("/FXML/GameMenu.fxml");
+        Pane root = FXMLLoader.load(url);
+        ApplicationController.setRootSize(root);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        ApplicationController.setStage(stage);
+        root.setBackground(new Background(ApplicationController.setBackGround("/someImages/board.jpg")));
+        stage.show();
+
+
+
+    }
 }
