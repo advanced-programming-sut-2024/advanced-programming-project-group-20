0ced61c6abfd7f9d151d583bc99088c1071efc45
ali <ali>
2024-06-16 00:42:19 +0330

    commit for eror of pull

--- Gwent/src/main/java/Controller/GameController.java
+++ Gwent/src/main/java/Controller/GameController.java
@@ -5,6 +5,7 @@ import Model.CardBuilder;
 import Model.Factions.Monsters;
 import Model.Factions.Nilfgaard;
 import Model.User;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -13,6 +14,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GameController {
 
